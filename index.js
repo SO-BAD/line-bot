a08c89403a799aec986040a39fedd9cc04ff5b68
hank <anima923@hotmail.com>
2022-06-12 00:22:44 +0800

    s

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
 import 'dotenv/config';
 import linebot from 'linebot';
 
+
 const bot = linebot({
     channelId: process.env.CHANNEL_ID,
     channelSecret: process.env.CHANNEL_SECRET,
-    channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+    channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
 });
 
-bot.on('message',function(event){
-    event.reply('event.message.text');
+bot.on('message',(event)=>{
     event.reply(event.message.text);
-    // event.reply(event.message.text)
-    // .then(function($data){
-
-    // })
-    // .catch(function(error){
-        
-    // })
 });
-console.log(process.env)
-bot.listen('/',process.env.PORT || 3000, ()=>{})
+
+bot.listen('/',process.env.PORT || 3000, ()=>{
+    console.log('das');
+})
